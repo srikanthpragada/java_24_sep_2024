9dcc17019b6cfdae9c28f237d471a850c20efc0d
srikanthpragada <srikanthpragada@gmail.com>
2024-10-09 18:56:56 +0530

    09-OCT

---
+++ demo/src/oop/TestShape.java
@@ -0,0 +1,56 @@
+package oop;
+
+abstract class Shape {
+	private int x, y;
+
+	public Shape(int x, int y) {
+		super();
+		this.x = x;
+		this.y = y;
+	}
+
+	public abstract int getArea();
+}
+
+class Square extends Shape {
+	private int side;
+
+	public Square(int x, int y, int side) {
+		super(x, y);
+		this.side = side;
+	}
+
+	@Override
+	public int getArea() {
+		return this.side * this.side;
+	}
+}
+
+class Rect extends Shape {
+	private int length, width;
+
+	public Rect(int x, int y, int length, int width) {
+		super(x, y);
+		this.length = length;
+		this.width = width;
+	}
+
+	@Override
+	public int getArea() {
+		return this.length * this.width;
+	}
+}
+
+public class TestShape {
+	public static void main(String[] args) {
+		Shape s;
+
+		s = new Square(10, 20, 15);
+		System.out.println(s.getArea());
+
+		s = new Rect(10, 10, 15, 20);
+		System.out.println(s.getArea());
+
+	}
+
+}
