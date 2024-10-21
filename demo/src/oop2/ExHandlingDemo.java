53b050d07da38232d80c90090ca27e56364400dc
srikanthpragada <srikanthpragada@gmail.com>
2024-10-21 18:53:35 +0530

    21-OCT

--- demo/src/oop2/ExHandlingDemo.java
+++ demo/src/oop2/ExHandlingDemo.java
@@ -3,13 +3,19 @@ package oop2;
 public class ExHandlingDemo {
 
 	public static void main(String[] args) {
-		 String value = "0";
-		 
-		 int v = Integer.parseInt(value);
-		 int r  = 100 / v;
-		 
-		 System.out.println(r);
+		String value = "0";
 
+		try {
+			int v = Integer.parseInt(value);
+			int r = 100 / v;
+			System.out.println(r);
+		} catch (NumberFormatException ex) {
+			System.out.println("Invalid Number!");
+		} 
+		finally {
+			System.out.println("Finally!");
+		}
+		System.out.println("The End!");
 	}
 
 }
