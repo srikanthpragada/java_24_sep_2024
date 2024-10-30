1e8ae5f946f72f3de0e13b3a35d0d449591b3dee
srikanthpragada <srikanthpragada@gmail.com>
2024-10-30 18:59:58 +0530

    30-OCT

--- demo/src/network/Download.java
+++ demo/src/network/Download.java
@@ -7,9 +7,9 @@ import java.net.URL;
 public class Download {
 
 	public static void main(String[] args) throws Exception {
-	 	URL urlobj = new URL("http://www.srikanthtechnologies.com/banner.jpg");
+	 	URL urlobj = new URL("https://www.srikanthtechnologies.com/banner.jpg");
 		InputStream is = urlobj.openStream();
-		FileOutputStream fo = new FileOutputStream("d:\\classroom\\st.jpg");
+		FileOutputStream fo = new FileOutputStream("c:\\classroom\\st.jpg");
 		int ch = is.read();
 		while (ch != -1) {
 			fo.write(ch);
